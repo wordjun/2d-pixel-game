acd6cbe5759dd6ec5c52bd04503f3ee1e09cc708
junhyunpark <wordjun@naver.com>
2021-02-02 23:58:39 +0900

    Vending machiune interaction added

---
+++ Assets/ObjectInteraction.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectInteraction : MonoBehaviour
+{
+    [SerializeField] private bool isInFrontOfObject;
+    public Animator animator;
+    void Start()
+    {
+        isInFrontOfObject = false;
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            isInFrontOfObject = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            isInFrontOfObject = false;
+        }
+    }
+
+    void Update()
+    {
+        if (isInFrontOfObject)
+        {
+            animator.SetBool("Interact", true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                animator.SetTrigger("Interact");
+            }
+        }
+    }
+}
---
+++ Assets/VendingMachine.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VendingMachine : MonoBehaviour
+{
+    private bool isInFront;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            isInFront = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            isInFront = false;
+        }
+    }
+    void Start()
+    {
+        isInFront = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInFront)
+        {
+
+        }
+    }
+}
