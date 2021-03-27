d0418c76567f903811a53074c3320d1fc82459e9
12161579@office.inha.ac.kr <12161579@office.inha.ac.kr>
2021-03-27 19:18:08 +0900

    Mar 27th

---
+++ Assets/Scripts/Monologues/FourthFloorScreamMonologue.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FourthFloorScreamMonologue : MonoBehaviour
+{
+    public Text guideText;
+    public Animator animator;
+    private bool isMonologueDone;
+    [SerializeField] private bool isInteracting;
+    void Start()
+    {
+        isInteracting = false;
+        isMonologueDone = false;
+        guideText.canvasRenderer.SetAlpha(0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            isInteracting = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            isInteracting = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isMonologueDone)
+        {
+            if (isInteracting)//if player is inside the colliderbox and hasn't started the monologue yet
+            {
+                animator.SetTrigger("Start");//set the animator trigger, start monologue animation
+                StartCoroutine(MonologueDone());
+            }
+        }
+        else if (isMonologueDone)
+        {
+            //animator.SetTrigger("End");//set the animator trigger, end the animation
+            guideText.canvasRenderer.SetAlpha(255);//pop up guidetext
+        }
+    }
+    IEnumerator MonologueDone()
+    {
+        yield return new WaitForSeconds(2.0f);
+        isMonologueDone = true;
+    }
+}
---
+++ Assets/Scripts/Monologues/ThirdFloorToiletMonologue.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThirdFloorToiletMonologue : MonoBehaviour
+{
+    public static bool isBodyGone;
+    private bool isDoorOpen;
+    public Animator animator;
+    public Text text;
+    [SerializeField] private bool isInteracting;
+    void Start()
+    {
+        isInteracting = false;
+        text.canvasRenderer.SetAlpha(0);
+        isDoorOpen = false;
+        isBodyGone = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            isInteracting = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            isInteracting = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInteracting)//if player is inside the colliderbox
+        {
+            animator.SetTrigger("Monologue1");//set the animator trigger, start monologue animation
+            text.canvasRenderer.SetAlpha(255);//trigger "OPEN" UI
+
+            if (!isDoorOpen && Input.GetKeyDown(KeyCode.E))//if the door is closed and the player entered "E" key to open it, start the next sequence
+            {
+                isDoorOpen = true;
+                text.canvasRenderer.SetAlpha(0);
+
+                Debug.Log("Entered E, door now opening.");
+                //dead woman inside toilet
+                animator.SetTrigger("Monologue2");
+            }
+        }
+        else if (!isInteracting)//if player's outside the colliderbox
+        {
+            text.canvasRenderer.SetAlpha(0);//make the "OPEN" UI disappear
+        }
+    }
+}
